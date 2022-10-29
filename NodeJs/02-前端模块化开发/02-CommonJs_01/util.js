5a7485ca9573abeae71a4b63b13937c61960e1f5
xionglp <785874752@qq.com>
2022-10-29 12:00:17 +0800

    commonJS导出

---
+++ NodeJs/02-前端模块化开发/02-CommonJs_01/util.js
@@ -0,0 +1,15 @@
+const UTIL_NAME = "util_name"
+
+function formatCount() {
+  return "200万"
+}
+
+function formatDate() {
+  return "2022-10-29"
+}
+
+// commonJs导出
+exports.UTIL_NAME = UTIL_NAME;
+exports.formatCount = formatCount;
+exports.formatDate = formatDate;
+
