6e0ef6fe3dd321bbec80ac356866b504bacd5f72
xionglp <785874752@qq.com>
2022-10-29 15:23:31 +0800

    没有模块化的情况

---
+++ NodeJs/02-前端模块化开发/01-没有模块化/aaa.js
@@ -0,0 +1,12 @@
+const moduleA = (function () {
+  const name = "xionglp"
+  const age = 18
+  const height = 1.88
+  console.log(name)
+
+  return {
+    name,
+    age,
+    height
+  }
+}())
---
+++ NodeJs/02-前端模块化开发/01-没有模块化/bbb.js
@@ -0,0 +1,4 @@
+(function () {
+  let name = "bbb"
+  console.log(name)
+}())
